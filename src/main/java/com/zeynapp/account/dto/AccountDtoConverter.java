23f9cfcb8f12529b5e1dddbcc86af3c917e4ec7c
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-11-24 14:16:04 +0300

    some adding process

--- src/main/java/com/zeynapp/account/dto/AccountDtoConverter.java
+++ src/main/java/com/zeynapp/account/dto/AccountDtoConverter.java
@@ -2,12 +2,14 @@ package com.zeynapp.account.dto;
 
 import com.zeynapp.account.model.Account;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Data
+@Component
 public class AccountDtoConverter {
 
     private final CustomerDtoConverter customerDtoConverter;
