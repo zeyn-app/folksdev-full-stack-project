0accb2667df91729535a32933323ff07351c355e
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-11-25 15:03:25 +0300

    a user defined as a costumer

--- src/main/java/com/zeynapp/account/AccountApplication.java
+++ src/main/java/com/zeynapp/account/AccountApplication.java
@@ -1,11 +1,30 @@
 package com.zeynapp.account;
 
+import com.zeynapp.account.model.Customer;
+import com.zeynapp.account.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication implements CommandLineRunner {
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+	}
+
+	@Override
+	public void run(String...args){
+		Customer customer = customerRepository.save(Customer
+				.builder()
+				.name("Zeynep")
+				.surname("Özdemir")
+				.build());
+		System.out.println(customer);
 	}
 }
