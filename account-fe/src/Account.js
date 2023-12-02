01a5a90a8b73124a52d1b1e42f6287f9d90c1f11
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-12-02 16:49:28 +0300

    Added form to display user information, renamed variables (id to customerId, amount to initialCredit), refactored state structure: createAccountRequest properties updated (id to customerId, amount to initialCredit)

--- account-fe/src/Account.js
+++ account-fe/src/Account.js
@@ -1,22 +1,32 @@
 import React, {Component} from 'react';
 import logo from "./logo.svg";
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 // /v1/accounts
 class Account extends Component{
-    state = {
-        account: {
-            transactions: []
-        }
-    };
 
     constructor(props) {
         super(props);
         this.state = {
             createAccountRequest:{
-                id: "",
-                amount: ""
+                customerId: "",
+                initialCredit: ""
             }
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.createAccountRequest};
+        // createAccountRequest[name] = value;
+        item[name] = value;
+        this.setState({createAccountRequest: item});
     }
 
 
@@ -24,16 +34,16 @@ class Account extends Component{
     async componentDidMount(){
         this.setState({
             createAccountRequest:{
-                id: this.props.match.params.id,
-                amount: ""
+                customerId: this.props.match.params.id,
+                initialCredit: ""
             }
             }
         )
     }
 
     async handleSubmit(event) {
-        event.preventDefault(); // butona basınca tüm sayfanın yenilenmesini engellliyor
-        const {item} = this.state;
+        event.preventDefault();
+        const { createAccountRequest } = this.state;
 
         await fetch('/v1/accounts', {
             method: 'POST',
@@ -41,9 +51,9 @@ class Account extends Component{
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(item),
+            body: JSON.stringify(createAccountRequest),
         });
-        this.props.history.push('/clients');
+        this.props.history.push('/');
     }
 
     render() {
@@ -54,7 +64,23 @@ class Account extends Component{
                     <img src={logo} className="App-logo" alt="logo" />
                     <div className="App-intro">
                         <h2>Create Account</h2>
-                        {createAccountRequest.id}
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Label for="customerId">Name</Label>
+                                <Input type="text" name="customerId" id="customerId" value={createAccountRequest.customerId || ''}
+                                       disabled/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="initialCredit">Initial Credit</Label>
+                                <Input type="text" name="initialCredit" id="initialCredit" value={createAccountRequest.initialCredit || ''}
+                                       onChange={this.handleChange} autoComplete="initialCredit"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Button color="primary" type="submit" to="/">Save</Button>{' '}
+                                {/*<Button color="primary" type="submit" onClick={() => this.props.history.push('/')}>Save</Button>{' '}*/}
+                                <Button color="secondary" tag={Link} to="/" >Cancel</Button>
+                            </FormGroup>
+                        </Form>
                         <div></div>
                     </div>
                 </header>
