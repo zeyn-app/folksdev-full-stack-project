08bb8f3525aca481e16c73966555c70f39e83fc3
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-12-02 16:51:50 +0300

    Refactored Customer component structure: - Removed duplicated imports - Updated state structure to use 'customer' and 'customers' properties - Adjusted fetch endpoint to retrieve all customers - Reorganized rendering logic for improved clarity

--- account-fe/src/Customer.js
+++ account-fe/src/Customer.js
@@ -1,57 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {Component} from "react";
-import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Link, Switch} from 'react-router-dom';
+import {Button} from 'reactstrap';
 
 
 class Customer extends Component {
     state = {
         // [] collection, {} an object
-        customer: {
-            accounts:[{
-                transactions:[]
+        // customer: {
+        //     accounts:[{
+        //         transactions:[]
+        //     }]
+        // }
+
+        customers: [{
+            accounts: [{
+                transactions: []
             }]
-        }
+
+        }]
     };
 
     async componentDidMount() {
-        const response = await fetch('/v1/customers/e0598665-6bf7-4a22-9ee8-564e3a9e644f');
+        const response = await fetch('/v1/customers');
         const body = await response.json();
-        this.setState({customer: body});
+        this.setState({customers: body});
     }
 
+    handleRefreshClick = () => {
+        // Reload the page
+        window.location.reload();
+    };
+
+
     render() {
-        const {customer} = this.state;
+        const {customers} = this.state;
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo" alt="logo"/>
                     <div className="App-intro">
                         <h2>Customer</h2>
-                        <div key={customer.id}>
-                            {customer.name} {customer.surname},
-                            {customer.accounts.map(account =>
-                                <p key={account.id}>
-                                    {account.balance}, ({account.creationDate}),
-                                    {account.transactions.map(transaction=>(
-                                        <div key={transaction.id}>
-                                            transactionInfo: {transaction.amount}, {transaction.transactionDate}, {transaction.transactionType}
-                                        </div>
-                                    ))}
-                                </p>)}
-                            <Router>
-                                <Switch>
-                                    <Button color="link"><Link to="/account">Account</Link></Button>
-                                </Switch>
-                            </Router>
-                        </div>
+                        {customers.map(customer =>
+                            <div key={customer.id}>
+                                {customer.name} {customer.surname},
+                                {customer.accounts.map(account =>
+                                    <p key={account.id}>
+                                        {account.balance}, ({account.creationDate}),
+                                        {account.transactions.map(transaction => (
+                                            <div key={transaction.id}>
+                                                transactionInfo: {transaction.amount}, {transaction.transactionDate}, {transaction.transactionType}
+                                            </div>
+                                        ))}
+                                    </p>)}
+                                <Router>
+                                    <Switch>
+                                        <Button color="link"  onClick={this.handleRefreshClick}>
+                                            <Link to={`/account/${customer.id}`}>Account</Link>
+                                        </Button>
+                                    </Switch>
+                                </Router>
+                            </div>
+                        )}
                     </div>
                 </header>
             </div>
         );
     }
 }
+
 export default Customer;
 
